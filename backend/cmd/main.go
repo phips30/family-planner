ac5250830e1695c9ce07fed38ac5419842df0e20
phips30 <wiesinger.philipp@gmail.com>
2025-03-03 20:24:38 +0100

    Use os instead of deprecated ioutil

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,38 +1,38 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "os"
+	"fmt"
+	"net/http"
+	"os"
 
-    "github.com/gorilla/mux"
-    user "family-planner/backend/internal/user"
-    "family-planner/backend/internal/db"
+	"family-planner/backend/internal/db"
+	"family-planner/backend/internal/user"
+
+	"github.com/gorilla/mux"
 )
 
 // To be defined via ENV vars
 const PORT = "8080"
 
 func main() {
-    dbpool := db.Connect()
-    defer dbpool.Close()
+	dbpool := db.Connect()
+	defer dbpool.Close()
 
-    fmt.Printf("Initializing database ...\n")
-    err := db.InitDb(dbpool)
-    if err != nil {
-        fmt.Printf("Error initializing database: %s\n", err)
-    }
-    fmt.Printf("Initializing database completed.\n")
+	fmt.Printf("Initializing database ...\n")
+	err := db.InitDb(dbpool)
+	if err != nil {
+		fmt.Printf("Error initializing database: %s\n", err)
+	}
+	fmt.Printf("Initializing database completed.\n")
 
+	r := mux.NewRouter()
 
-    r := mux.NewRouter()
+	user.RegisterRoutes(r)
 
-    user.RegisterRoutes(r)
-
-    fmt.Printf("Starting server on port %s\n", PORT)
-    err = http.ListenAndServe(":" + PORT, r)
-    if err != nil {
-        fmt.Printf("Error starting server: %s\n", err)
-        os.Exit(1)
-    }
+	fmt.Printf("Starting server on port %s\n", PORT)
+	err = http.ListenAndServe(":"+PORT, r)
+	if err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+		os.Exit(1)
+	}
 }
--- backend/internal/db/db.go
+++ backend/internal/db/db.go
@@ -1,47 +1,45 @@
 package db
 
 import (
-    "context"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "os"
-    "path/filepath"
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
 
-    "github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 const DB_CONNECTION_STRING = "postgres://postgres:postgres@localhost:5432/family_planner"
 
 func Connect() *pgxpool.Pool {
-    dbpool, err := pgxpool.New(context.Background(), DB_CONNECTION_STRING)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
-        os.Exit(1)
-    }
-    return dbpool
+	dbpool, err := pgxpool.New(context.Background(), DB_CONNECTION_STRING)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
+		os.Exit(1)
+	}
+	return dbpool
 }
 
 func InitDb(pool *pgxpool.Pool) error {
-    sqlfiles, err := filepath.Glob("../db/sql/*.sql")
-    if err != nil {
-        log.Fatal(err)
-    }
+	sqlfiles, err := filepath.Glob("../db/sql/*.sql")
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+	}
 
-    for _, sqlfile := range sqlfiles {
-        data, err := ioutil.ReadFile(sqlfile)
-        if err != nil {
-            log.Fatal(err)
-        }
+	for _, sqlfile := range sqlfiles {
+		data, err := os.ReadFile(sqlfile)
+		if err != nil {
+			fmt.Printf("err: %v\n", err)
+		}
 
-        sql := string(data)
-        _, err = pool.Exec(context.Background(), sql)
-        if err != nil {
-            log.Fatal(err)
-        }
-        log.Printf("Executed SQL script: %s\n", sqlfile)
+		sql := string(data)
+		_, err = pool.Exec(context.Background(), sql)
+		if err != nil {
+			fmt.Printf("err: %v\n", err)
+		} else {
+			fmt.Printf("Executed SQL script: %s\n", sqlfile)
+		}
+	}
 
-    }
-
-    return nil
+	return nil
 }
